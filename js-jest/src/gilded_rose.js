71751204860be469901034deb40d96172a75ae1e
arion aranda <arioncronos15s@gmail.com>
2026-04-23 16:08:33 -0600

    refactor(js): quitar condicionales anidados y hacer mas clara la actualizacion de calidad

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -18,12 +18,12 @@ class Shop {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
 
-      this._updateItemQuality(item);
-
-      if (!this._isSulfuras(item)) {
-        item.sellIn = item.sellIn - 1;
+      if (this._isSulfuras(item)) {
+        continue;
       }
 
+      this._updateItemQuality(item);
+      item.sellIn = item.sellIn - 1;
       this._updateExpiredItemQuality(item);
     }
 
@@ -31,57 +31,62 @@ class Shop {
   }
 
   _updateItemQuality(item) {
+    if (this._isAgedBrie(item)) return this._updateAgedBrie(item);
+    if (this._isBackstagePass(item)) return this._updateBackstagePass(item);
+
+    this._updateNormalItem(item);
+  }
+
+  _updateNormalItem(item) {
+    if (item.quality <= 0) return;
+
+    this._decreaseQuality(item);
+  }
+
+  _updateAgedBrie(item) {
+    if (item.quality >= 50) return;
+
+    this._increaseQuality(item);
+  }
+
+  _updateBackstagePass(item) {
+    if (item.quality >= 50) return;
+
+    this._increaseQuality(item);
+
+    if (item.sellIn < 11) {
+      this._increaseQuality(item);
+    }
+
+    if (item.sellIn < 6) {
+      this._increaseQuality(item);
+    }
+  }
+
+  _updateExpiredItemQuality(item) {
+    if (item.sellIn >= 0) {
+      return;
+    }
+
     if (this._isAgedBrie(item)) {
       this._updateAgedBrie(item);
       return;
     }
 
     if (this._isBackstagePass(item)) {
-      this._updateBackstagePass(item);
+      item.quality = 0;
       return;
     }
 
     this._updateNormalItem(item);
   }
 
-  _updateNormalItem(item) {
-    if (item.quality > 0 && !this._isSulfuras(item)) {
-      item.quality = item.quality - 1;
-    }
+  _increaseQuality(item) {
+    item.quality = Math.min(50, item.quality + 1);
   }
 
-  _updateAgedBrie(item) {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-    }
-  }
-
-  _updateBackstagePass(item) {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-      if (item.sellIn < 11 && item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
-      if (item.sellIn < 6 && item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
-    }
-  }
-
-  _updateExpiredItemQuality(item) {
-    if (item.sellIn < 0) {
-      if (this._isAgedBrie(item)) {
-        this._updateAgedBrie(item);
-        return;
-      }
-
-      if (this._isBackstagePass(item)) {
-        item.quality = item.quality - item.quality;
-        return;
-      }
-
-      this._updateNormalItem(item);
-    }
+  _decreaseQuality(item) {
+    item.quality = Math.max(0, item.quality - 1);
   }
 
   _isAgedBrie(item) {
